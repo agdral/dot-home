6097e9127eda27788171565d20b3195735695baa
agdral <ezequiel.escobar@proton.me>
2025-08-18 04:26:33 -0300

    feat(fish): add 'sutup' alias for updating multiple flakes
    Adds a new fish shell alias 'sutup' that updates three specific flakes
    (dotfile-default, home-default, nixvim-config) and then performs a
    nixos-rebuild switch in one command.

--- shell/fish/aliases/nixos.nix
+++ shell/fish/aliases/nixos.nix
@@ -2,6 +2,13 @@
   programs.fish.functions = {
     f-all = "cd /etc/nixos/ && sudo nix flake update";
     f-one = "cd /etc/nixos/ && sudo nix flake lock update $argv";
+    sutup = ''
+      cd /etc/nixos/
+      sudo nix flake lock update dotfile-default
+      sudo nix flake lock update home-default
+      sudo nix flake lock update nixvim-config
+      sudo nixos-rebuild switch
+    '';
     sudup = "sudo nixos-rebuild switch";
     suboot = "sudo nixos-rebuild boot";
     surmg = "sudo nix-collect-garbage -d";
