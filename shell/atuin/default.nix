246382142392626a2d0eee35e1d591d3af3cef59
agdral <ezequiel.escobar@proton.me>
2025-08-21 07:28:57 -0300

    fix atuin

--- shell/atuin/default.nix
+++ shell/atuin/default.nix
@@ -1,19 +1,21 @@
 {...}: {
-  programs.atuin = {
-    enable = true;
-    enableFishIntegration = true;
-    settings = {
-      update_check = false;
-      enter_accept = true;
-      show_help = false;
-      show_tabs = false;
-      style = "full";
-      inline_height = 20;
+  programs = {
+    atuin = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        update_check = false;
+        enter_accept = true;
+        show_help = false;
+        show_tabs = false;
+        style = "full";
+        inline_height = 20;
+      };
     };
-  };
-  fish = {
-    interactiveShellInit = ''
-      bind up _atuin_bind_up
-    '';
+    fish = {
+      interactiveShellInit = ''
+        bind up _atuin_bind_up
+      '';
+    };
   };
 }
