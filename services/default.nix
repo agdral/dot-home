4c63f7f5b21d2ebdfe2e91589ee5bace528e24a3
agdral <ezequiel.escobar@proton.me>
2025-09-02 18:45:19 -0300

    revert modele config

--- services/default.nix
+++ services/default.nix
@@ -1,34 +1,14 @@
+{...}:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-with lib; let
-  moduleOption = [
-    "hypridle"
-    "musicWake"
-    # "openTablet"
-    "safeEyes"
-    "wlsunset"
-    "espanso"
-    "swww"
-    "syncthing"
-    "hyprPolkit"
-    "pyprland"
+  imports = [
+    ./hyprPolkit
+    ./pyprland
+    ./swww
+    ./syncthing
   ];
-in {
-  options = genAttrs moduleOption (name: mkEnableOption name);
 
-  config = lib.mkMerge ([
-      {
-        services.copyq.enable = true;
-        services.gnome-keyring.enable = true;
-      }
-    ]
-    ++ map (name:
-      lib.mkIf config.${name}.enable (
-        (import ./${name}) {inherit config lib pkgs;}
-      ))
-    moduleOption);
+  services = {
+    copyq.enable = true;
+    gnome-keyring.enable = true;
+  };
 }
