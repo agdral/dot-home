ec86a8e37bb58a046a28126fd40ee9988e0ae53c
agdral <ezequiel.escobar@proton.me>
2025-09-10 22:17:39 -0300

    add dependences tidal-dl-ng

--- packages/tidal/tidal-ng.nix
+++ packages/tidal/tidal-ng.nix
@@ -3,6 +3,17 @@
   buildPythonApplication,
   fetchPypi,
   requests,
+  mutagen,
+  dataclasses-json,
+  pathvalidate,
+  m3u8,
+  coloredlogs,
+  rich,
+  toml,
+  typer,
+  tidalapi,
+  python-ffmpeg,
+  pycryptodome,
 }:
 buildPythonApplication rec {
   pname = "tidal_dl_ng";
@@ -14,7 +25,20 @@ buildPythonApplication rec {
     sha256 = "091y2vplb7hyn7gz00902h47n8wkvhn52xh5hc3cgwrqby8h5mn0";
   };
 
-  propagatedBuildInputs = [requests];
+  propagatedBuildInputs = [
+    requests
+    mutagen
+    dataclasses-json
+    pathvalidate
+    m3u8
+    coloredlogs
+    rich
+    toml
+    typer
+    tidalapi
+    python-ffmpeg
+    pycryptodome
+  ];
 
   meta = with lib; {
     description = "A downloader for Tidal music service";
