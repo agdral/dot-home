592398544a0621d2531e23834e2e3517691a5e6b
agdral <ezequiel.escobar@proton.me>
2025-09-17 14:49:39 -0300

    fix hash proton

--- packages/proton/default.nix
+++ packages/proton/default.nix
@@ -4,13 +4,12 @@
 
     src = pkgs.fetchurl {
       url = "https://proton.me/download/pass/linux/x64/proton-pass_1.32.6_amd64.deb";
-      hash = "1gv92plbklkxhyzxv4pn19m9dr72757krr8s0329gmy75w96vnn6";
+      sha256 = "1gv92plbklkxhyzxv4pn19m9dr72757krr8s0329gmy75w96vnn6";
     };
   });
 in {
   home.packages = with pkgs; [
     proton-pass-new
-
     protonmail-desktop
   ];
 }
