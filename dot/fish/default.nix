d85ab2fe964af28369ca0431f17c22cbedfa6075
agdral <ezequiel.escobar@proton.me>
2025-09-29 00:34:13 -0300

    fix module fish

--- dot/fish/default.nix
+++ dot/fish/default.nix
@@ -1,19 +1,9 @@
-{
-  config,
-  lib,
-  tools,
-  ...
-}:
-with lib; let
-  cfg = config.dotShell;
-in {
-  config = mkIf cfg.fish {
-    imports = tools.importNixFiles ./functions [];
-    programs.fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set -g fish_greeting
-      '';
-    };
+{tools, ...}: {
+  imports = tools.importNixFiles ./functions [];
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set -g fish_greeting
+    '';
   };
 }
