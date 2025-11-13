bef012316ef9fa9c9f999700131d0c8021c85279
agdral <ezequiel.escobar@proton.me>
2025-11-13 11:40:29 -0300

    update praton mail

--- packages/proton/default.nix
+++ packages/proton/default.nix
@@ -14,11 +14,20 @@ with lib; let
       sha256 = "0wda2cxf7ddawkr96ydjxmnwyihp6h3y01vv8gac9iygpfw0pv1s";
     };
   });
+
+  proton-mail-new = pkgs.protonmail-desktop.overrideAttrs (oldAttrs: {
+    version = "1.10.1";
+
+    src = pkgs.fetchurl {
+      url = "https://proton.me/download/mail/linux/1.10.1/ProtonMail-desktop-beta.deb";
+      sha256 = "0gphc7dsz0795qyl0l6cmvz51b7p9aacrziw42abahb2k9q1pgay";
+    };
+  });
 in {
   config = mkIf cfg.proton {
-    home.packages = with pkgs; [
+    home.packages = [
       proton-pass-new
-      protonmail-desktop
+      proton-mail-new
     ];
   };
 }
