737ead13e9f3dfa8ecd3b3ee2093e1d31daef0be
agdral <ezequiel.escobar@proton.me>
2026-01-22 17:15:01 -0300

    fix settings and theme yazi config

--- dot/yazi/settings.nix
+++ dot/yazi/settings.nix
@@ -1,52 +1,50 @@
-{...}: {
-  programs.yazi.settings = {
-    mgr = {
-      show_hidden = true;
-      show_symlink = true;
-      sort_by = "natural";
-      title_format = "";
-      sort_sensitive = false;
-      sort_dir_first = true;
-    };
+{
+  mgr = {
+    show_hidden = true;
+    show_symlink = true;
+    sort_by = "natural";
+    title_format = "";
+    sort_sensitive = false;
+    sort_dir_first = true;
+  };
 
-    opener = {
-      edit = [
-        {
-          run = ''neovide --wayland_app_id=neovide "$@" -- -c "cd $@"'';
-          desc = "Neovide";
-          block = true;
-          for = "unix";
-        }
-        {
-          run = ''nohup code "$@" &'';
-          desc = "Code";
-          block = true;
-          for = "unix";
-        }
-      ];
-    };
+  opener = {
+    edit = [
+      {
+        run = ''neovide --wayland_app_id=neovide "$@" -- -c "cd $@"'';
+        desc = "Neovide";
+        block = true;
+        for = "unix";
+      }
+      {
+        run = ''nohup code "$@" &'';
+        desc = "Code";
+        block = true;
+        for = "unix";
+      }
+    ];
+  };
 
-    plugin = {
-      prepend_preloaders = [
-        {
-          mime = "/data/kael/Music/Music/**";
-          run = "exifaudio";
-        }
-      ];
-      prepend_previewers = [
-        {
-          mime = "audio/*";
-          run = "exifaudio";
-        }
-        {
-          mime = "application/epub+zip";
-          run = "epub-preview";
-        }
-        {
-          mime = "application/bittorrent";
-          run = "torrent-preview";
-        }
-      ];
-    };
+  plugin = {
+    prepend_preloaders = [
+      {
+        mime = "/data/kael/Music/Music/**";
+        run = "exifaudio";
+      }
+    ];
+    prepend_previewers = [
+      {
+        mime = "audio/*";
+        run = "exifaudio";
+      }
+      {
+        mime = "application/epub+zip";
+        run = "epub-preview";
+      }
+      {
+        mime = "application/bittorrent";
+        run = "torrent-preview";
+      }
+    ];
   };
 }
