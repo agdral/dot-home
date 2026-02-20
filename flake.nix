617e9f63d1de749250a1c2c172857a1c3be077be
agdral <ezequiel.escobar@proton.me>
2026-02-20 18:34:08 -0300

    add input nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   description = "Home Dotfiles";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    import-tree.url = "github:vic/import-tree";
+  };
 
   outputs = {
-    lib,
+    nixpkgs,
     import-tree,
     ...
-  }: {
+  }: let
+    lib = nixpkgs.lib;
+  in {
     homeModules = {
       packages = import-tree.filter (lib.hasSuffix "/default.nix") ./packages;
       services = import-tree.filter (lib.hasSuffix "/default.nix") ./services;
