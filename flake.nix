1293dff056b6b18c3f003c34ec511dbe749fcccb
agdral <ezequiel.escobar@proton.me>
2026-02-22 16:59:09 -0300

    add module firefox addons

--- flake.nix
+++ flake.nix
@@ -3,19 +3,29 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     import-tree.url = "github:vic/import-tree";
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     import-tree,
+    firefox-addons,
     ...
   }: let
     lib = nixpkgs.lib;
   in {
-    homeModules = {
-      packages = import-tree.filter (lib.hasSuffix "/default.nix") ./packages;
-      services = import-tree.filter (lib.hasSuffix "/default.nix") ./services;
-      shell = import-tree.filter (lib.hasSuffix "/default.nix") ./shell;
+    homeModules.packages = {
+      _module.args = {
+        inherit firefox-addons;
+      };
+      imports = [
+        (import-tree.filter (lib.hasSuffix "/default.nix") ./packages)
+      ];
     };
+    homeModules.services = import-tree.filter (lib.hasSuffix "/default.nix") ./services;
+    homeModules.shell = import-tree.filter (lib.hasSuffix "/default.nix") ./shell;
   };
 }
