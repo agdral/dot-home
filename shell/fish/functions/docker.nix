485f8025c2f8dcb20f4314ebc55b304966a34110
agdral <ezequiel.escobar@proton.me>
2026-02-25 01:58:25 -0300

    remove syncthing module, remove function docker-if in fish

--- shell/fish/functions/docker.nix
+++ shell/fish/functions/docker.nix
@@ -1,13 +1,4 @@
 {
-  docker-if = ''
-    set i_service (docker inspect --format '{{.State.Running}}' $argv[1])
-    if test "$i_service" = true
-        docker compose -f $argv[2] stop
-    else
-        docker compose -f $argv[2] up -d
-        firefox -p argonarch $argv[3]
-    end
-  '';
   docks = "docker ps -a --format 'table {{.ID}}\\t{{.Names}}\\t{{.Ports}}'";
   dc = "docker compose $argv";
   dv = "docker volume $argv";
