79fa8fd1a0f0b2b7c024160fa7e52be2bd29eec7
agdral <ezequiel.escobar@proton.me>
2026-02-27 21:48:06 -0300

    add functions in docker shell

--- shell/fish/functions/docker.nix
+++ shell/fish/functions/docker.nix
@@ -2,4 +2,11 @@
   docks = "docker ps -a --format 'table {{.ID}}\\t{{.Names}}\\t{{.Ports}}'";
   dc = "docker compose $argv";
   dv = "docker volume $argv";
+  dv-copy = ''
+    docker run --rm -v $argv[1]:/from -v $argv[2]:/to alpine \
+    sh -c "cd /from && cp -av . /to"
+  '';
+  dv-view = ''
+    docker run -it --rm -v $argv[1]:/data alpine sh
+  '';
 }
