4561093913926b5f0ce094d42391416b72779c22
agdral <ezequiel.escobar@proton.me>
2026-05-04 21:48:05 -0300

    update proton packages

--- packages/proton/default.nix
+++ packages/proton/default.nix
@@ -8,15 +8,15 @@ with lib; let
   cfg = config.dotPack;
   proton-pass-new = pkgs.proton-pass.overrideAttrs (oldAttrs: {
     src = pkgs.fetchurl {
-      url = "https://proton.me/download/pass/linux/x64/proton-pass_1.36.0_amd64.deb";
-      sha256 = "0v922ryjzfi6vwh6xyaq4grcwaqzz29galhgpmn2qiwj91ihz63c";
+      url = "https://proton.me/download/pass/linux/x64/proton-pass_1.36.1_amd64.deb";
+      sha256 = "1q2r7yk1hdjv9hpwj6f956ai9yi6q6n0m59iy4p1amq3c18vban3";
     };
   });
 
   proton-mail-new = pkgs.protonmail-desktop.overrideAttrs (oldAttrs: {
     src = pkgs.fetchurl {
-      url = "https://proton.me/download/mail/linux/1.12.1/ProtonMail-desktop-beta.deb";
-      sha256 = "10djrivg5z5xjr8ikq4mpk7n36hmrdm6xmqbasa7cccgxpycpnh8";
+      url = "https://proton.me/download/mail/linux/1.13.0/ProtonMail-desktop-beta.deb";
+      sha256 = "0xggin2ggvfjm8y7138chv7kzwrkajmq0ibg6qpa964mx68w66vs";
     };
   });
 in {
